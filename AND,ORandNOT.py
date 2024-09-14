d276a451375bde6d113d7a13c9dd3cdb0922bc21
salma201231 <s.abddaoui@gmail.com>
2024-09-14 12:24:38 +0100

    initial commit

---
+++ AND,ORandNOT.py
@@ -0,0 +1,13 @@
+a=16
+b=53
+if a>0 and b>0:
+    print("positives numbers")
+else:
+    print("not positives numbers")
+a=54
+b=-43
+if a>0 or b>0:
+    print("1number should be positive")
+else:
+    print("no nuber is positive")
+    
